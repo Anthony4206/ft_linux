dbfa63d68d1a4c5823a6a95a5f6876de476b6ddc
Your Name <you@example.com>
2025-02-07 11:46:19 +0100

    First

---
+++ partition.sh
@@ -0,0 +1,30 @@
+### PARTITIONING ###
+# fdisk: manipulate disk partition table
+#   - n: create a partition
+#        - p: primary
+#	      - Select first sector (default)
+#	      - Select last sector: boot: +200M, swap: +2G, root: the rest
+#        - e: extended
+#   - a: toggle a bootable flag
+#        - Choose the /boot partition
+#   - t: change a partition type
+#        - Choose the /swap partition
+#        - 82: Linux swap
+#   - p: print the partition table
+#   - w: write table to disk and exit
+
+### FORMATTING ###
+# mkfs: build a linux filesystem
+#   - sudo mkfs.ext4 /dev/sdb3: format root with ext4 (fourth extended filesystem) 
+#   - sudo mkfs.ext4 /dev/sdb1: format boot with ext4
+# mkswap: set up a Linux swap area
+#   - sudo mkswap /dev/sdb2: format swap (an exchange partition)
+#   - sudo swapon /dev/sdb2: activate swap
+
+### MOUNTING  ###
+export LFS=/mnt/lfs
+# mount: mount a filesystem (allows to operate a disk or partition)
+sudo mkdir -p $LFS
+sudo mount -t ext4 /dev/sdb3 $LFS
+sudo mkdir -p $LFS/boot
+sudo mount -t ext4 /dev/sdb1 $LFS/boot
