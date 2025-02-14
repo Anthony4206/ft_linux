ff261df18bd4f2b9497867f88cf91d8ab3bc26d2
Anthony <anthony.levasseur@ymail.com>
2025-02-14 09:31:35 +0100

    host_requirement done

--- host_requirements.sh
+++ host_requirements.sh
@@ -4,7 +4,7 @@
 # If you have tools installed in other directories, adjust PATH here AND
 # in ~lfs/.bashrc (section 4.4) as well.
 
-sudo apt intall -y bison flex texinfo gawk m4
+sudo apt install -y bison flex texinfo gawk m4
 
 LC_ALL=C 
 PATH=/usr/bin:/bin
@@ -16,29 +16,29 @@ sort   /dev/null || bail "sort does not work"
 
 ver_check()
 {
-   if ! type -p $2 &>/dev/null
-   then 
-     echo "ERROR: Cannot find $2 ($1)"; return 1; 
-   fi
-   v=$($2 --version 2>&1 | grep -E -o '[0-9]+\.[0-9\.]+[a-z]*' | head -n1)
-   if printf '%s\n' $3 $v | sort --version-sort --check &>/dev/null
-   then 
-     printf "OK:    %-9s %-6s >= $3\n" "$1" "$v"; return 0;
-   else 
-     printf "ERROR: %-9s is TOO OLD ($3 or later required)\n" "$1"; 
-     return 1; 
-   fi
+	if ! type -p $2 &>/dev/null
+	then
+		echo "ERROR: Cannot find $2 ($1)"; return 1;
+	fi
+	v=$($2 --version 2>&1 | grep -E -o '[0-9]+\.[0-9\.]+[a-z]*' | head -n1)
+	if printf '%s\n' $3 $v | sort --version-sort --check &>/dev/null
+	then
+		printf "OK:    %-9s %-6s >= $3\n" "$1" "$v"; return 0;
+	else
+		printf "ERROR: %-9s is TOO OLD ($3 or later required)\n" "$1"
+		return 1;
+	fi
 }
 
 ver_kernel()
 {
-   kver=$(uname -r | grep -E -o '^[0-9\.]+')
-   if printf '%s\n' $1 $kver | sort --version-sort --check &>/dev/null
-   then 
-     printf "OK:    Linux Kernel $kver >= $1\n"; return 0;
-   else 
-     printf "ERROR: Linux Kernel ($kver) is TOO OLD ($1 or later required)\n" "$kver"; 
-     return 1; 
+	kver=$(uname -r | grep -E -o '^[0-9\.]+')
+	if printf '%s\n' $1 $kver | sort --version-sort --check &>/dev/null
+	then
+		printf "OK:    Linux Kernel $kver >= $1\n"; return 0;
+	else
+		printf "ERROR: Linux Kernel ($kver) is TOO OLD ($1 or later required)\n" "$kver";
+		return 1;
    fi
 }
 
@@ -71,11 +71,11 @@ then echo "OK:    Linux Kernel supports UNIX 98 PTY";
 else echo "ERROR: Linux Kernel does NOT support UNIX 98 PTY"; fi
 
 alias_check() {
-   if ! $1 --version 2>&1 | grep -qi $2
-   then
-	   sudo ln -sf $(which $3) $(which $1)
-	 printf "OK:    %-4s is $2\n" "$1"
-   else printf "OK:    %-4s is $2\n" "$1"; fi
+	if ! $1 --version 2>&1 | grep -qi $2
+	then
+		sudo ln -sf $(which $3) $(which $1)
+		printf "OK:    %-4s is $2\n" "$1"
+	else printf "OK:    %-4s is $2\n" "$1"; fi
 }
 echo "Aliases:"
 alias_check awk GNU gawk
@@ -89,7 +89,7 @@ else echo "ERROR: g++ does NOT work"; fi
 rm -f a.out
 
 if [ "$(nproc)" = "" ]; then
-   echo "ERROR: nproc is not available or it produces empty output"
+	echo "ERROR: nproc is not available or it produces empty output"
 else
-   echo "OK:    nproc reports $(nproc) logical cores are available"
+	echo "OK:    nproc reports $(nproc) logical cores are available"
 fi
