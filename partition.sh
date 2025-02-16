8d9a52e67922c9dc41f390639431c5a59cc45737
Anthony <anthony.levasseur@ymail.com>
2025-02-16 21:07:51 +0100

    save

--- partition.sh
+++ partition.sh
@@ -24,16 +24,21 @@ echo -e "n\np\n1\n\n+200M\n\
 
 ### FORMATTING ###
 # mkfs: build a linux filesystem
-sudo mkfs.ext4 /dev/sdb3 # Format root with ext4 (fourth extended filesystem) 
-sudo mkfs.ext4 /dev/sdb1: format boot with ext4
+sudo mkfs -vt ext4 /dev/sdb3 # Format root with ext4 (fourth extended filesystem) 
+sudo mkfs -vt ext4 /dev/sdb1: format boot with ext4
 # mkswap: set up a Linux swap area
 sudo mkswap /dev/sdb2: format swap (an exchange partition)
 sudo swapon /dev/sdb2: activate swap
-
 ### MOUNTING  ###
 export LFS=/mnt/lfs
+echo "export LFS=/mnt/lfs" >> /home/alevasse/.bashrc
+echo "export LFS=/mnt/lfs" | sudo tee -a /root/.bashrc > /dev/null
 # mount: mount a filesystem (allows to operate a disk or partition)
-sudo mkdir -p $LFS
-sudo mount -t ext4 /dev/sdb3 $LFS
-sudo mkdir -p $LFS/boot
-sudo mount -t ext4 /dev/sdb1 $LFS/boot
+sudo mkdir -vp $LFS
+sudo mount -vt ext4 /dev/sdb3 $LFS
+sudo mkdir -vp $LFS/boot
+sudo mount -Vt ext4 /dev/sdb1 $LFS/boot
+echo "/dev/sdb3	/mnt/lfs	ext4	defaults	1	1" \
+| sudo tee -a /etc/fstab > /dev/null 
+echo "/dev/sdb1	/mnt/lfs/boot	ext4	defaults	1	1" \
+| sudo tee -a /etc/fstab > /dev/null
