4b22c95fc32daa27ea20debb291810b3e9ee95f0
Anthony <anthony.levasseur@ymail.com>
2025-02-21 16:35:35 +0100

    add sudo

--- scripts/finals.sh
+++ scripts/finals.sh
@@ -17,7 +17,7 @@ sudo passwd -d lfs
 sudo cp ./lfs/bash_profile /home/lfs/.bash_profile
 sudo cp ./lfs/bashrc /home/lfs/.bashrc
 sudo cp ./lfs/run_lfs.sh /home/lfs/
-mkdir -pv /home/lfs/scripts
+sudo mkdir -pv /home/lfs/scripts
 sudo cp ./lfs/scripts/tmp_system.sh /home/lfs/scripts
 sudo chown lfs:lfs /home/lfs/{.bash_profile,.bashrc,run_lfs.sh}
 sudo chown lfs:lfs /home/lfs/scripts/tmp_system.sh
