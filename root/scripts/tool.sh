a8931ea7bd88f8dc5fdabf81e11b74e120cd1d0e
Anthony <anthony.levasseur@ymail.com>
2025-03-22 10:05:39 +0100

    debug

--- root/scripts/tool.sh
+++ root/scripts/tool.sh
@@ -1,17 +1,4 @@
-xauth:  file /root/.serverauth.30338 does not exist
-xauth: (argv):1:  bad display name "debian:0" in "list" command
-xauth: (stdin):1:  bad display name "debian:0" in "add" command
-
-_XSERVTransSocketUNIXCreateListener: ...SocketCreateListener() failed
-_XSERVTransMakeAllCOTSServerListeners: server already running
-(EE) 
-Fatal server error:
-(EE) Cannot establish any listening sockets - Make sure an X server isn't already running(EE) 
-(EE) 
-Please consult the The X.Org Foundation support 
-	 at http://wiki.x.org
- for help. 
-(EE) Please also check the log file at "/var/log/Xorg.0.log" for additional information.
-(EE) 
-(EE) Server terminated with error (1). Closing log file.
+root       627  0.3  0.5 491012 138304 tty7    Ssl+ Mar18  18:55 /usr/lib/xorg/Xorg :0 -seat seat0 -auth /var/run/lightdm/root/:0 -nolisten tcp vt7 -novtswitch
+1000      1001  0.0  0.0  11192  2872 ?        Ss   Mar18   0:00 /bin/sh /etc/xdg/xfce4/xinitrc -- /etc/X11/xinit/xserverrc
+root     30391  0.0  0.0 215160  2100 ?        S+   10:03   0:00 grep --color=auto X
 
